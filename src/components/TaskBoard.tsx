66471ddc00ea3492ce7653e08db1c875c94861b7
Vadim Savin <savinvadim1312@gmail.com>
2024-04-12 18:28:47 +0100

    reorder

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -2,10 +2,36 @@ import { View, StyleSheet, SafeAreaView } from 'react-native';
 import TaskList from './TaskList';
 import { LinearGradient } from 'expo-linear-gradient';
 import TaskDragArea from './TaskDragArea';
+import { BSON } from 'realm';
+import { useRealm } from '@realm/react';
+import { Task } from '../models/Task';
+import { ItemHeight } from './TaskListItem';
 
 export default function TaskBoard() {
+  const realm = useRealm();
+
+  const updateItemPosition = (itemId: BSON.ObjectID, y: number) => {
+    const newPosition = Math.max(1, Math.ceil((y - 73) / ItemHeight));
+
+    const allTasks = realm.objects(Task).sorted('position');
+    const allTasksArr = Array.from(allTasks);
+
+    const fromIndex = allTasks.findIndex(
+      (i) => i._id.toString() === itemId.toString()
+    );
+
+    const element = allTasksArr.splice(fromIndex, 1)[0];
+    allTasksArr.splice(newPosition - 1, 0, element);
+
+    realm.write(() => {
+      allTasksArr.forEach((task, index) => {
+        task.position = index + 1;
+      });
+    });
+  };
+
   return (
-    <TaskDragArea>
+    <TaskDragArea updateItemPosition={updateItemPosition}>
       <View style={{ padding: 10, flex: 1 }}>
         <LinearGradient
           // Background Linear Gradient
--- src/components/TaskDragArea.tsx
+++ src/components/TaskDragArea.tsx
@@ -21,22 +21,37 @@ const DraggingContext = createContext<DraggingContext>({
   draggingTaskId: null,
 });
 
-const TaskDragArea = ({ children }: PropsWithChildren) => {
+const TaskDragArea = ({
+  children,
+  updateItemPosition,
+}: PropsWithChildren<{
+  updateItemPosition: (id: BSON.ObjectID, y: number) => void;
+}>) => {
   const [draggingTaskId, setDraggingTaskId] = useState<BSON.ObjectID>(null);
-
   const { width } = useWindowDimensions();
 
   const dragX = useSharedValue(0);
   const dragY = useSharedValue(0);
 
+  const drop = () => {
+    updateItemPosition(draggingTaskId, dragY.value);
+    setDraggingTaskId(null);
+  };
+
   const pan = Gesture.Pan()
     .manualActivation(true)
     .onTouchesMove((event, stateManager) => {
-      stateManager.activate();
+      if (draggingTaskId) {
+        stateManager.activate();
+      }
     })
     .onChange((event) => {
       dragX.value = dragX.value + event.changeX;
       dragY.value = dragY.value + event.changeY;
+    })
+    .onEnd(() => {
+      console.log('Dropped');
+      runOnJS(drop)();
     })
     .onFinalize(() => {
       runOnJS(setDraggingTaskId)(null);
